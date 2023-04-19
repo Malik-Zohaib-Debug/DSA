f972a0f041e357f5cb424c00280a4d8559373ce6
Malik Zohaib Zahid <zohaibzahid112233@gmail.com>
2023-04-19 15:40:38 +0500

    added search
    implemented search function in tries

--- Tries/tries.cpp
+++ Tries/tries.cpp
@@ -2,59 +2,92 @@
 #include <string>
 using namespace std;
 
-class TrieNode {
+class TrieNode
+{
 public:
-  char data;
-  TrieNode *children[26];
-  bool isTerminal;
+    char data;
+    TrieNode *children[26];
+    bool isTerminal;
 
 public:
-  TrieNode(char ch) {
-    data = ch;
-    for (int i = 0; i < 26; i++) {
-      children[i] = NULL;
+    TrieNode(char ch)
+    {
+        data = ch;
+        for (int i = 0; i < 26; i++)
+        {
+            children[i] = NULL;
+        }
+        isTerminal = false;
     }
-    isTerminal = false;
-  }
 };
 
-class Trie {
+class Trie
+{
 public:
-  TrieNode *root;
+    TrieNode *root;
 
 public:
-  Trie() { root = new TrieNode('\0'); }
-  void insertWord(string word);
-  void insertUtil(TrieNode *root, string word);
-  
+    Trie() { root = new TrieNode('\0'); }
+    void insertWord(string word);
+    void insertUtil(TrieNode *root, string word);
+    bool searchWord(string word);
+    bool searchUtil(TrieNode *root, string word);
 };
 void Trie::insertWord(string word) { insertUtil(root, word); }
-void Trie::insertUtil(TrieNode *root, string word) {
-  // base case
-  if (word.length() == 0) {
-    root->isTerminal = true;
-    return;
-  }
-  // processing
-  int index = word[0] - 'A';
-  TrieNode *child;
-  if (root->children[index] != NULL) {
-    child = root->children[index];
-  } else {
-    child = new TrieNode(word[0]);
-    root->children[index] = child;
-  }
-  // recursion
-  insertUtil(child, word.substr(1));
+void Trie::insertUtil(TrieNode *root, string word)
+{
+    // base case
+    if (word.length() == 0)
+    {
+        root->isTerminal = true;
+        return;
+    }
+    // processing
+    int index = word[0] - 'A';
+    TrieNode *child;
+    if (root->children[index] != NULL)
+    {
+        child = root->children[index];
+    }
+    else
+    {
+        child = new TrieNode(word[0]);
+        root->children[index] = child;
+    }
+    // recursion
+    insertUtil(child, word.substr(1));
 }
-int main() {
-  Trie *t = new Trie();
+bool Trie::searchUtil(TrieNode *root, string word)
+{
+    if (word.length() == 0)
+    {
+        return root->isTerminal;
+    }
+    int index = word[0] - 'A';
+    TrieNode *child;
+    if (root->children[index] != NULL)
+    {
+        child = root->children[index];
+    }
+    else
+    {
+        return false;
+    }
+    return searchUtil(child, word.substr(1));
+}
+bool Trie::searchWord(string word)
+{
+    return searchUtil(root, word);
+}
+int main()
+{
+    Trie *t = new Trie();
 
-  t->insertWord("ARM");
-  t->insertWord("DO");
-  t->insertWord("TIME");
+    t->insertWord("ARM");
+    t->insertWord("DO");
+    t->insertWord("TIME");
 
-  
+    cout << "Present or Not -> " << t->searchWord("ARM");
 
-  return 0;
+    return 0;
 }
