46c7f4c607a9925f7f55ab46c58e29d150f01cc5
Malik-Zohaib-Debug <zohaibzahid112233@gmail.com>
2025-01-08 18:31:46 +0500

    added linear search.cpp

---
+++ Leetcode/Array/LinearSearch.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+
+bool linearSearch(int *arr, int size, int ele){
+	for(int i=0; i<size; i++){
+		if(arr[i] == ele){
+			return true;
+		}
+	}
+	return false;
+}
+
+int main(){
+	int arr[] = {2,4,1,0,-1,3};
+	int size = sizeof(arr)/sizeof(int);
+	
+	cout << linearSearch(arr, size, 3);
+	
+	
+	return 0;
+}
